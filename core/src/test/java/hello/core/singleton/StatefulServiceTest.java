0fbaf59988e28655a841c9471ac5d5cf11b88e14
hj-language <idgafff010@gmail.com>
2024-03-19 22:48:42 +0900

    5-4. 싱글톤 방식의 주의점 - 문제

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // 쓰레드A에서 A 사용자가 1만원 주문
+        statefulService1.order("userA", 10000);
+        // 쓰레드B에서 B 사용자가 2만원 주문
+        statefulService2.order("userB", 20000);
+
+        // 쓰레드A에서 A 사용자가 주문 금액을 조회한다.
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
