50e1eb67fae3e13c587aca6e5882815b9da9b370
hj-language <idgafff010@gmail.com>
2024-03-20 23:40:51 +0900

    6-3. 필터

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 해당 패키지부터 탐색
+        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지부터 탐색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {}
