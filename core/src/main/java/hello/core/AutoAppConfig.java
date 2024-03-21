510f5f0c2d5823014ebea55a91732d8383440109
hj-language <idgafff010@gmail.com>
2024-03-21 21:50:29 +0900

    6-4. 중복 등록과 충돌
    - 이전 예제인 [자동 빈 등록 vs. 자동 빈 등록] 예제 롤백
    - [수동 빈 등록 vs. 자동 빈 등록] 예제 추가
      - 스프링 부트 애플리케이션에서는 충돌 시 에러 => application.properties에서 설정 변경 가능 (default false)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
         basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지부터 탐색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig {}
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,7 +6,7 @@ import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("service")
+@Component()
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
