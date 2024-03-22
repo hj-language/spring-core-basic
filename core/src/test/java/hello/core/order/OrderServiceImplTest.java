56bcc576caa5d971bcd93b6280b76b18f0be0d73
hj-language <idgafff010@gmail.com>
2024-03-22 22:30:21 +0900

    7-3. 생성자 주입을 선택해라! - 수정자 주입 시 NullPointerException 예제
    - 테스트 코드를 짜는 입장에서는 수정자 기반 의존관계 주입 시 의존관계를 파악하기 어려움

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,11 @@
+package hello.core.order;
+
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+    @Test
+    void createOrder() {
+        OrderServiceImpl orderService = new OrderServiceImpl();
+        orderService.createOrder(1L, "itemA", 10000);
+    }
+}
